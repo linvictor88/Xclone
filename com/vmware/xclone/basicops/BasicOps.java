a24ffb64b1d285fd679d059a401bc1ad90d49d8d
dujizhi <jizhid@vmware.com>
2013-07-30 10:32:28 +0800

    modified BasicOps.java

--- com/vmware/xclone/basicops/BasicOps.java
+++ com/vmware/xclone/basicops/BasicOps.java
@@ -141,12 +141,30 @@ public class BasicOps
 	}
 }
 
+class  CloneVM {
+
+	private static String[] meTree = {
+		"ManagedEntity",
+		"ComputeResource",
+		"ClusterComputeResource",
+		"Datacenter",
+		"Folder",
+		"HostSystem",
+		"ResourcePool",
+		"VirtualMachine"
+	};
+	private static String[] crTree = {
+		"ComputeResource",
+		"ClusterComputeResource"
+	};
+	private static String[] hcTree = {
+		"HistoryCollector",
+		"EventHistoryCollector",
+		"TaskHistoryCollector"
+	};
 
 
-public class CloneVM {
-
-
-	private  ManagedObjectReference propCollectorRef;
+	private ManagedObjectReference propCollectorRef;
 	private  ManagedObjectReference rootRef;
 	private  VimService vimService;
 	private  VimPortType vimPort;
@@ -166,9 +184,9 @@ public class CloneVM {
 
 	private ManagementObjects managementObject;
 	private  boolean powerOn= false;
+	private boolean template= false;
 
-
-	public CloneVM(String vmPathName,String cloneName,String targetIp,String targetDS,String targetPool,boolean powerOn)
+	public CloneVM(String vmPathName,String cloneName,String targetIp,String targetDS,String targetPool,boolean powerOn,VCConnection vcConn)
 	{
 
 		this.vmPathName =  vmPathName;
@@ -178,106 +196,73 @@ public class CloneVM {
 		this.targetPool = targetPool;
 		this.powerOn 	= powerOn;
 
+
+		this.propCollectorRef = vcConn.getPropRef();
+		this.rootRef  = vcConn.getRootRef();
+		this.serviceContent = vcConn.getServiceContent();
+		this.vimPort = vcConn.getVimPort();
+		this.vimService = vcConn.getVimService();
+
 		managementObject = ManagementObjects.getInstance();
 	};
 
+
+	private static boolean typeIsA(String searchType,
+			String foundType) {
+		if(searchType.equals(foundType)) {
+			return true;
+		} else if(searchType.equals("ManagedEntity")) {
+			for(int i = 0; i < meTree.length; ++i) {
+				if(meTree[i].equals(foundType)) {
+					return true;
+				}
+			}
+		} else if(searchType.equals("ComputeResource")) {
+			for(int i = 0; i < crTree.length; ++i) {
+				if(crTree[i].equals(foundType)) {
+					return true;
+				}
+			}
+		} else if(searchType.equals("HistoryCollector")) {
+			for(int i = 0; i < hcTree.length; ++i) {
+				if(hcTree[i].equals(foundType)) {
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+
 	public boolean doClone()
 	{
-
-
-
-
-		return true;
-	}
-
-	private  VirtualMachineCloneSpec  getCloneSpec()
-	{
-		VirtualMachineCloneSpec cloneSpec = new VirtualMachineCloneSpec();
-
 		try
 		{
-			//	InetAddress addr = InetAddress.getByName( VMClone.targetIp);
-
-			VirtualMachineRelocateSpec relocSpec = new VirtualMachineRelocateSpec();
-
-			cloneSpec.setLocation(relocSpec);
-			cloneSpec.setPowerOn(true);
-			cloneSpec.setTemplate(false);
-
-			if(targetIp!=null&&!(targetIp.equals("")))
-			{
-				ManagedObjectReference  target  =managementObject.getHostByIp(targetIp);
-
-				ManagedObjectReference datastoreRef = managementObject.getDataStore(targetIp);
-
-				List<DynamicProperty> datastoresSource
-				= getDynamicProarray(target, "HostSystem", "datastore");
-				ArrayOfManagedObjectReference dsSourceArr =
-						((ArrayOfManagedObjectReference) (datastoresSource.get(0)).getVal());
-
-				List<ManagedObjectReference> dsTarget = dsSourceArr.getManagedObjectReference();
-
-				ManagedObjectReference dsMOR =browseDSMOR(dsTarget);
-
-				if(dsMOR == null) {
-					throw new IllegalArgumentException(" DataSource "
-							+ "dest" + " Not Found.");
-				}
-
-
-				ManagedObjectReference poolMOR = getDecendentMoRef(null, "ResourcePool", "cluster");
-
-
-				relocSpec.setHost(target);
-
-				relocSpec.setDatastore(dsMOR);
-
-				relocSpec.setPool(poolMOR);
-			}
-
-
-		}catch(Exception e)
+			return execCloneVM();
+		}
+		catch(Exception e)
 		{
 			e.printStackTrace();
-
-			return null;
 		}
 
-		return cloneSpec;
+
+		return false;
 	}
 
 
-	private  void execCloneVM() throws Exception {
+
+	private  boolean execCloneVM() throws Exception {
 		// Find the Datacenter reference by using findByInventoryPath().
 		try 
 		{
-
-
 			ManagedObjectReference datacenterRef  = managementObject.getDataCenterByName(dataCenterName);
 
-			if (datacenterRef == null) {
-				System.out.printf(
-						"The specified datacenter [ %s ]is not found %n", dataCenterName);
-				return;
-			}
 			ManagedObjectReference folderRef = managementObject.getFolderInDatacenter(dataCenterName);
-			if (folderRef == null) {
-				System.out.println("The virtual machine is not found");
-				return;
-			}
-
-			managementObject.getVMByName(vmPathName);
-
-			ManagedObjectReference vmRef
-			= vimPort.findByInventoryPath(serviceContent.getSearchIndex(), vmPathName);
-			if (vmRef == null) {
-				System.out.printf("The VMPath specified [ %s ] is not found %n", vmPathName);
-				return;
-			}
-
-			VirtualMachineCloneSpec cloneSpec = getCloneSpec();
 
 
+			ManagedObjectReference vmRef =   managementObject.getVMByPathname(vmPathName);
+
+			//String targetIp,String vmPathName,String targetPool,boolean powerOn,boolean template)
+			VirtualMachineCloneSpec cloneSpec = managementObject.getCloneSpec(targetIp,vmPathName,targetPool,powerOn,template);
 
 			ManagedObjectReference cloneTask
 			= vimPort.cloneVMTask(vmRef, folderRef, cloneName, cloneSpec);
@@ -295,14 +280,14 @@ public class CloneVM {
 				// Wait till the task completes.
 				if (results[0].equals(TaskInfoState.SUCCESS)) {
 					System.out.printf(
-							"Successfully cloned Virtual Machine [%s] to clone name [%s] %n",
+							"Successfully cloned Virtual Machine [%s] to clone name [%s] %n \n",
 							vmPathName.substring(vmPathName.lastIndexOf("/") + 1), cloneName);
 
-
+					return true;
 					//this.cloneName
 				} else {
 					System.out.printf(
-							"Failure Cloning Virtual Machine [%s] to clone name [%s] %n",
+							"Failure Cloning Virtual Machine [%s] to clone name [%s] %n \n",
 							vmPathName.substring(vmPathName.lastIndexOf("/") + 1), cloneName);
 				}
 			}
@@ -311,8 +296,120 @@ public class CloneVM {
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+
+
+		return false;
 	}
 
+
+
+	private  static void updateValues(String[] props, Object[] vals, PropertyChange propchg) {
+		for (int findi = 0; findi < props.length; findi++) {
+			if (propchg.getName().lastIndexOf(props[findi]) >= 0) {
+				if (propchg.getOp() == PropertyChangeOp.REMOVE) {
+					vals[findi] = "";
+				} else {
+					vals[findi] = propchg.getVal();
+				}
+			}
+		}
+	}
+	public    Object[] waitForValues(ManagedObjectReference objmor,
+			String[] filterProps,
+			String[] endWaitProps,
+			Object[][] expectedVals)
+					throws RemoteException,
+					Exception {
+		// version string is initially null
+		String version = "";
+		Object[] endVals = new Object[endWaitProps.length];
+		Object[] filterVals = new Object[filterProps.length];
+
+		PropertyFilterSpec spec = new PropertyFilterSpec();
+
+		spec.getObjectSet().add(new ObjectSpec());
+
+		spec.getObjectSet().get(0).setObj(objmor);
+
+		spec.getPropSet().addAll(Arrays.asList(new PropertySpec[]{new PropertySpec()}));
+
+		spec.getPropSet().get(0).getPathSet().addAll(Arrays.asList(filterProps));
+
+		spec.getPropSet().get(0).setType(objmor.getType());
+
+		//spec.getObjectSet().get(0).getSelectSet().add(null);
+
+		spec.getObjectSet().get(0).setSkip(Boolean.FALSE);
+
+		ManagedObjectReference filterSpecRef = vimPort.createFilter(propCollectorRef, spec, true);
+
+		boolean reached = false;
+
+		UpdateSet updateset = null;
+		PropertyFilterUpdate[] filtupary = null;
+		PropertyFilterUpdate filtup = null;
+		ObjectUpdate[] objupary = null;
+		ObjectUpdate objup = null;
+		PropertyChange[] propchgary = null;
+		PropertyChange propchg = null;
+		while (!reached) {
+			boolean retry = true;
+			while (retry) {
+				try {
+					updateset = vimPort.waitForUpdates(propCollectorRef, version);
+					retry = false;
+				} catch (SOAPFaultException sfe) {
+					sfe.printStackTrace();
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+			if(updateset != null) {
+				version = updateset.getVersion();
+			}
+			if (updateset == null || updateset.getFilterSet() == null) {
+				continue;
+			}
+			List<PropertyFilterUpdate> listprfup = updateset.getFilterSet();
+			filtupary = listprfup.toArray(new PropertyFilterUpdate[listprfup.size()]);
+			filtup = null;
+			for (int fi = 0; fi < filtupary.length; fi++) {
+				filtup = filtupary[fi];
+				List<ObjectUpdate> listobjup = filtup.getObjectSet();
+				objupary = listobjup.toArray(new ObjectUpdate[listobjup.size()]);
+				objup = null;
+				propchgary = null;
+				for (int oi = 0; oi < objupary.length; oi++) {
+					objup = objupary[oi];
+					if (objup.getKind() == ObjectUpdateKind.MODIFY
+							|| objup.getKind() == ObjectUpdateKind.ENTER
+							|| objup.getKind() == ObjectUpdateKind.LEAVE) {
+						List<PropertyChange> listchset = objup.getChangeSet();
+						propchgary
+						= listchset.toArray(new PropertyChange[listchset.size()]);
+						for (int ci = 0; ci < propchgary.length; ci++) {
+							propchg = propchgary[ci];
+							updateValues(endWaitProps, endVals, propchg);
+							updateValues(filterProps, filterVals, propchg);
+						}
+					}
+				}
+			}
+			Object expctdval = null;
+			// Check if the expected values have been reached and exit the loop if done.
+			// Also exit the WaitForUpdates loop if this is the case.
+			for (int chgi = 0; chgi < endVals.length && !reached; chgi++) {
+				for (int vali = 0; vali < expectedVals[chgi].length
+						&& !reached; vali++) {
+					expctdval = expectedVals[chgi][vali];
+					reached = expctdval.equals(endVals[chgi]) || reached;
+				}
+			}
+		}
+		// Destroy the filter when we are done.
+		vimPort.destroyPropertyFilter(filterSpecRef);
+		return filterVals;
+	}
 
 
 }
