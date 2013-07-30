e0ae5cc332dc1de8a5f71f169ca309b671ada6cc
Qinghe Jin <qjin@vmware.com>
2013-07-30 00:04:13 -0700

    fix srcVMName when do LinkedClone on a new host

--- com/vmware/xclone/algorithm/SimpleDeploy.java
+++ com/vmware/xclone/algorithm/SimpleDeploy.java
@@ -2,19 +2,28 @@ package com.vmware.xclone.algorithm;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 public class SimpleDeploy {
 	private String srcHost;
 	private String srcVMName;
 	private List<String> dstHostList;
+	private String prefix;
+	private Map<String, String> hostVMList;
 	private int numOfVm;
+	
+	private String CreateVMName(int numth) {
+		return prefix + String.format("%03d", numth);
+	}
 	
 	public SimpleDeploy(String srcHost, ArrayList<String> dstList,
 			String srcVmPath, int num, String prefix) {
 		this.srcHost = srcHost;
 		this.srcVMName = srcVmPath;
+		this.hostVMList.put(srcHost, srcVMName);
 		this.dstHostList = new ArrayList<String>(dstList);
 		this.numOfVm = num;
+		this.prefix = prefix;
 	}
 
 	public void run() {
@@ -38,14 +47,15 @@ public class SimpleDeploy {
 													1,
 													numStart, 
 													false);
+				hostVMList.put(dstHostList.get(i), CreateVMName(numStart));
 				fullClone.start();
 				try {
 					fullClone.wait();
 					DeployOneHost linkedClone = new DeployOneHost(
-													srcVMName,
+													hostVMList.get(dstHostList.get(i)),
 													dstHostList.get(i),
 													numPerHost - 1,
-													numStart,
+													numStart + 1,
 													true);
 					linkedClone.start();
 				} catch (InterruptedException e) {
